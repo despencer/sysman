e659b8eb048931275a3493ddb6bc5b851a42fdf1
despencer <despencer@mail.ru>
2023-12-30 22:32:41 +0300

    Right mouse setting added.

--- windows/makevm.bat
+++ windows/makevm.bat
@@ -72,7 +72,7 @@ echo [+] creating vm
 echo [+] configuring basic vm settings 1
 "%vboxmanage%" modifyvm %vm_name% --memory %vm_memory% --vram 64 --ioapic on --acpi on --chipset ich9 --largepages on --usb %vm_usb%> nul
 echo [+] configuring basic vm settings 2
-"%vboxmanage%" modifyvm %vm_name% --rtcuseutc on > nul
+"%vboxmanage%" modifyvm %vm_name% --rtcuseutc on --mouse usbtablet > nul
 echo [+] configuring network
 "%vboxmanage%" modifyvm %vm_name% --nictype1 82540EM --nic1 bridged --bridgeadapter1 "%vboxnetadapter%" > nul
 echo [+] configuring booting
