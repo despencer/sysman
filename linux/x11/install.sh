5ca39bfdb59df901dc76358b9bc71e8c4fc15927
despencer <despencer@mail.ru>
2023-12-31 21:06:43 +0300

    Added code for X11 with openbox installation.

--- linux/x11/install.sh
+++ linux/x11/install.sh
@@ -1,9 +1,13 @@
 log="x11.log"
+NAME="X11"
 
 source ../environ.sh
 
 _start_log
 _run_fore "Updating package base" "sudo pacman -Sy"
+_run_fore "Installing X11" "sudo pacman -S xorg-server openbox xorg-xinit xterm ttf-liberation --noconfirm"
+_run_cmd "Setting xinit" "echo 'exec openbox' > ~/.xinitrc"
+_run_cmd "Configuring openbox" "cp -r openbox.config ~/.config/openbox"
 
 echo "See log in $log"
 
