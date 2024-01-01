63832d304744f92dcb220ee3132b56995f68fa81
despencer <despencer@mail.ru>
2024-01-01 19:56:48 +0300

    Misc fonts for X added.

--- linux/x11/install.sh
+++ linux/x11/install.sh
@@ -5,7 +5,7 @@ source ../environ.sh
 
 _start_log
 _run_fore "Updating package base" "sudo pacman -Sy"
-_run_fore "Installing X11" "sudo pacman -S xorg-server openbox xorg-xinit xterm ttf-liberation --noconfirm"
+_run_fore "Installing X11" "sudo pacman -S xorg-server openbox xorg-xinit xterm ttf-liberation xorg-fonts-misc --noconfirm"
 _run_cmd "Setting xinit" "echo 'exec openbox' > ~/.xinitrc"
 _run_cmd "Configuring openbox" "cp -r openbox.config ~/.config/openbox"
 
