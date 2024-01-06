ff96bd3b20df95def7a9291f233584350078ae74
despencer <despencer@mail.ru>
2024-01-06 23:02:51 +0300

    Drivers and cli for pulseview added.

--- linux/embedded/install.sh
+++ linux/embedded/install.sh
@@ -5,7 +5,9 @@ source ../environ.sh
 
 _start_log
 _run_fore "Updating package base" "sudo pacman -Sy"
-_run_fore "Installing packages" "sudo pacman -S usbutils pulseview --noconfirm"
+
+# pulseview sigrok-cli sigrok-firmware-fx2lafw - logic analyzer packages with firmware
+_run_fore "Installing packages" "sudo pacman -S usbutils pulseview sigrok-cli sigrok-firmware-fx2lafw --noconfirm"
 _run_fore "Adding menu" "../x11/addmenu.sh PulseView /usr/bin/pulseview"
 
 echo "See log in $log"
