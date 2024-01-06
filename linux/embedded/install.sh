9fb6dfeaf86759bac714459761b0985c165bab04
despencer <despencer@mail.ru>
2024-01-06 23:57:41 +0300

    Add tools for embedded development.

--- linux/embedded/install.sh
+++ linux/embedded/install.sh
@@ -10,5 +10,12 @@ _run_fore "Updating package base" "sudo pacman -Sy"
 _run_fore "Installing packages" "sudo pacman -S usbutils pulseview sigrok-cli sigrok-firmware-fx2lafw --noconfirm"
 _run_fore "Adding menu" "../x11/addmenu.sh PulseView /usr/bin/pulseview"
 
+# development packages
+_run_fore "Installing dev packages" "sudo pacman -S make cmake arm-none-eabi-gcc arm-none-eabi-newlib libopencm3 --noconfirm"
+
+# dev tools packages
+_run_fore "Installing dev tools" "sudo pacman -S python-jinja python-yaml --noconfirm"
+_run_fore "Installing freertos" "git clone https://github.com/FreeRTOS/FreeRTOS-Kernel ~/.local/lib/freertos"
+
 echo "See log in $log"
 
