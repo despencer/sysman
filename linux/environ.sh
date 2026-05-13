cc499bdbac594c5aaf007e979c493adc92686e9e
despencer <despencer@mail.ru>
2026-05-13 23:05:52 +0300

    Remake X11 installation via ssh

--- linux/environ.sh
+++ linux/environ.sh
@@ -1,13 +1,18 @@
 USER='master'
 
 _start_log() {
- rm $log &> /dev/null
- date > $log
+ if [ "$TARGET" != '' ]; then
+   rm $log &> /dev/null
+   date > $log
+ else
+  echo "Usage: $0 target"
+  exit 1
+ fi
 }
 
 _run_cmd() {
  echo "================= $1" >> $log
- eval "$2" &>> $log
+ ssh $TARGET $2 &>> $log
  if [ "$?" -ne 0 ]; then
     echo "Fail $2 with $?"
     echo "See log in " $log
@@ -19,9 +24,21 @@ _run_cmd() {
 
 _run_fore() {
  echo "================= $1" >> $log
- eval "$2"
+ ssh $TARGET $2
  if [ "$?" -ne 0 ]; then
     echo "Fail $2 with $?"
+    exit 1
+ else
+    echo "[$NAME] $1"
+ fi
+}
+
+_run_copy() {
+ echo "================= $1" >> $log
+ scp -r $2 $TARGET:$3 &>> $log
+ if [ "$?" -ne 0 ]; then
+    echo "Fail copy $2 to $3 with $?"
+    echo "See log in " $log
     exit 1
  else
     echo "[$NAME] $1"
--- linux/x11/install.sh
+++ linux/x11/install.sh
@@ -1,13 +1,16 @@
 log="x11.log"
 NAME="X11"
+TARGET=$1
 
-source ../environ.sh
+CPATH=$(dirname $(realpath "$0"))
+source $CPATH/../environ.sh
 
 _start_log
 _run_fore "Updating package base" "sudo pacman -Sy"
 _run_fore "Installing X11" "sudo pacman -S xorg-server openbox xorg-xinit xterm ttf-liberation xorg-fonts-misc --noconfirm"
-_run_cmd "Setting xinit" "echo 'exec openbox' > ~/.xinitrc"
-_run_cmd "Configuring openbox" "cp -r openbox.config ~/.config/openbox"
+_run_cmd "Setting xinit 1" "touch ~/.Xauthority"
+_run_cmd "Setting xinit 2" "echo 'exec openbox' > ~/.xinitrc"
+_run_copy "Configuring openbox" "$CPATH/openbox.config" "~/.config/openbox"
 
 echo "See log in $log"
 
